ab2a901a3d18395df0a1d10637c3c654a086c971
Chandu-4444 <chandrakiran.g19@iiits.in>
2021-06-09 15:33:21 +0530

    Added AWS S3 Bucket support

--- CollabTree/settings.py
+++ CollabTree/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'backend',
     'taggit',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -126,12 +127,31 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
+
+
+# STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+AWS_ACCESS_KEY_ID = 'AKIAUIXD7J2DCZHZC3PK'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'collabtree-bucket'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400'
+}
+
+AWS_LOCATION = 'static'
+# AWS_PUBLIC_MEDIA_LOCATION = 'media'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+
 
 
 LOGIN_REDIRECT_URL = "dashboard"
@@ -146,6 +166,7 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'collabtree.team@gmail.com' 
 EMAIL_HOST_PASSWORD = 'CollabTree1234' 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DEFAULT_FILE_STORAGE = 'CollabTree.storage_backends.MediaStorage'
 
 
